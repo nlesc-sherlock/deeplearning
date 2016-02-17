1074ee92b2d29626ea61eca36a23886f8def60c9
lars <l.ridder@esciencecenter.nl>
2016-02-17 21:01:58 +0100

    Sqlite file carmodels.db containing 100 image urls per model (#21). Obtained by performing search_bing.py twice, using offset = 50 the second time. Had to remove special characters from carmodels.csv

--- scripts/search_bing.py
+++ scripts/search_bing.py
@@ -4,8 +4,8 @@ import sqlite3
 
 keyBing = 'MeAuVCPpeae5Lflf9dmJD0EU3Pe/KHH/tlbyTijGDik'        # get Bing key from: https://datamarket.azure.com/account/keys
 credentialBing = 'Basic ' + (':%s' % keyBing).encode('base64')[:-1] # the "-1" is to remove the trailing "\n" which encode adds
-top = 100
-offset = 0
+top = 50
+offset = 51
 
 def search_bing(keyword):
     searchString = '%27' + keyword + '%27'
@@ -26,8 +26,8 @@ curs = conn.cursor()
 #curs.execute("CREATE TABLE images (make TEXT, model TEXT, url TEXT, title TEXT)")
 #conn.commit()
 
-i=309
-for row in open('../results/carmodels.csv', 'r').readlines()[309:]:
+i=0
+for row in open('../results/carmodels.csv', 'r').readlines()[0:]:
     i+=1
     make,model,frequence = row[:-1].split(',')
     if make == "make": # header line
