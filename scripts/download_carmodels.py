c1652b3a91b090e0cea7f2c2e6189447ef9173aa
Christiaan Meijer <c.meijer@esciencecenter.nl>
2016-05-10 16:58:16 +0200

    car url downloader generates its own filenames (url number)

--- scripts/download_carmodels.py
+++ scripts/download_carmodels.py
@@ -8,20 +8,26 @@ curs = conn.cursor()
 #f = open("urls_for_lotsacars.txt")
 i=0
 for row in open('../carmodels.csv', 'r').readlines():
-    i+=1
     make,model,frequence = row[:-1].split(',')
     if make == "make": # header line
         continue
     r=curs.execute("select url from images where make = ? and model = ?", (unicode(make),unicode(model))).fetchall()
-    carmodel = make+'_'+model 
-    os.mkdir(carmodel)
+    carmodel = make+'_'+model
+
+    if os.path.exists(carmodel) is False:
+        os.mkdir(carmodel)
+
     for x in r:
+        i+=1
+        target = carmodel + '/' + str(i)
+        if os.path.exists(target):
+            continue
         try:
             url = x[0]
             filename = wget.download(url)
-            print filename,i
-            os.rename(filename, carmodel+'/'+filename)
+            print(filename + str(i))
+            os.rename(filename, target)
         except:
             pass
-    i+=1
+
 
