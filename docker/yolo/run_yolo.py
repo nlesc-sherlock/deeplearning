b4a50dc352ce6028809b3fd0028cc1cdb6bbbeda
cwmeijer <c.meijer@esciencecenter.nl>
2016-12-12 17:36:04 +0100

    add json in and out for yolo

--- docker/yolo/run_yolo.py
+++ docker/yolo/run_yolo.py
@@ -21,19 +21,19 @@ The output will be another json file.
         "classes":{
             "car" : [
                 {
-                    "id" : "/path/to/image",
+                    "path" : "/path/to/image",
                     "probability" : <float>,
                     "bbox" : [x, y, w, h]
                 },
                 {
-                    "id" : "/and/another/image",
+                    "path" : "/and/another/image",
                     "probability" : <float>,
                     "bbox" : [x, y, w, h]
                 }
             ],
             "person" : [
                 {
-                    "id" : "/and/another/image",
+                    "path" : "/and/another/image",
                     "probability" : <float>,
                     "bbox" : [x, y, w, h]
                 }
@@ -43,26 +43,54 @@ The output will be another json file.
 }
 
 Usage:std
-  run_yolo.py <jsonpath>
+  run_yolo.py <input_path> <output_path>
 """
 from docopt import docopt
 import json
 import subprocess
 
+
+def line_to_occurrence(path, line):
+    occurrence = {'path':path}
+    for pair in line.split(','):
+        key, value = pair.split(':')
+        key = key.strip(" '")
+        value = value.strip(" '")
+        if key == 'class':
+            clas = value
+        if key == 'prob':
+            occurrence['probability'] = float(value)
+        if key == 'left':
+            left = int(value)
+        if key == 'top':
+            top = int(value)
+        if key == 'right':
+            right = int(value)
+        if key == 'bottom':
+            bottom = int(value)
+    bbox = [left, top, right - left, top - bottom]
+    occurrence['bbox'] = bbox
+    return clas, occurrence
+
+
 if __name__ == '__main__':
     args = docopt(__doc__)
-    input = json.loads(args['<jsonpath>'])
-    for input_path in input['files']:
-        command = ["./darknet", "detect", "cfg/yolo.cfg", "yolo.weights", input_path]
-        temp_path = 'output.tmp'
-        with open(temp_path, 'w') as output_file:
-            result = subprocess.call(command, stdout=output_file)
-        with open(temp_path, 'r') as output_file:
-            elements = {}
-            for line in output_file:
+    with open(args['<input_path>'], 'r') as json_file:
+        json_input = json.load(json_file)
+        classes = {}
+        for input_path in json_input['files']:
+            command = ["./darknet", "detect", "cfg/yolo.cfg", "yolo.weights", input_path]
+            temp_path = 'output.tmp'
+            result = subprocess.check_output(command)
+            for line in result.split('\n'):
                 if line.startswith("'class':"):
-                    occurence = {}
-                    for key, value in line.split(','):
-                        if key in ['class', 'prob']
-                        occurence[key], value
+                    clas, occurrence = line_to_occurrence(input_path, line)
+                    if clas in classes:
+                        classes[clas].append(occurrence)
+                    else:
+                        classes[clas] = [occurrence]
+        json_output = json_input
+        json_output['classes'] = classes
 
+    with open(args['<output_path>'], 'w') as output_file:
+        json.dump(json_output, output_file, indent=4, sort_keys=True, ensure_ascii=False)
