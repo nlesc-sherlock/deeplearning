f37361fa91368e9f6a918272a460a823bdaa58f6
elboyran <e.ranguelova@esciencecenter.nl>
2016-12-12 16:53:32 +0100

    specification of the JSON input/output. The info is also at JSON/JSONschema_component_after_cropper.

--- scripts/cnn_classify.py
+++ scripts/cnn_classify.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-The input JSON for this script comes from the cropper component and looks like:
+The input JSON for a component comming after the cropper component looks like:
 
 {
     "files" : [
@@ -31,14 +31,124 @@ The input JSON for this script comes from the cropper component and looks like:
                     "cropped_image": "/yet/another/cropped/image"
                 }
             ]
+            "dog" : [
+                {
+                    "path" : "/yet/another/image",
+                    "probability" : <float>,
+                    "bbox" : [x, y, w, h],
+                    "cropped_image": "/yet/another/cropped/image"
+                }
+            ]            
         }
     }
 }
 
-In addition, the script takes two more arguments: <class (string)> and
-<threshold (number)>, e.g. "car" and "0.8".
+For the Bounding Boxes, the (x,y)  coordinates refer to the top left corner of 
+the bounding box.
 
-Output JSON is the same, but added to each object a classifier description:
+In addition, the script takes two more arguments: <class (list of string)> and
+<threshold (number)>, e.g. "car" and "0.8". The class ilist of strings are the
+class keys of interest in the input JSON file and the threshold is the minimum 
+class probability above which the class probabilities should be reported in the
+output JSON file.
+
+The output JSON enriches the input JSON with a classifier results for each BBox/
+cropped_image:
+
+{
+    "files" : [
+        "/path/to/image",
+        "/and/another/image"
+    ],
+    {
+        "classes":{
+            "car" : [
+                {
+                    "path" : "/path/to/image",
+                    "probability" : <float>,
+                    "bbox" : [x, y, w, h],
+                    "cropped_image": "/path/to/cropped/image",
+                     "classification": [
+                        "classifier": "car/model/name",
+                        "tags": [
+                            {"name": "Ford Fiesta",
+                             "probability": <float>
+                            },
+                            {"name": "Opel Astra",
+                             "pobability": <float>
+                            }
+                        ],                    
+                        "classifier": "car/color/name",
+                        "tags": [
+                            {"name": "white",
+                             "probability": <float>
+                            },
+                            {"name": "gray",
+                             "probability": <float>
+                            }
+                        ]                                            
+                    ]
+                },
+                {
+                    "path" : "/and/another/image",
+                    "probability" : <float>,
+                    "bbox" : [x, y, w, h],
+                    "cropped_image": "/another/cropped/image",
+                     "classification": [
+                        "classifier": "gender/model/name",
+                        "tags": [
+                            {"name": "f",
+                             "probability": <float>
+                            },
+                            {"name": "m",
+                             "pobability": <float>
+                            }
+                        ],                    
+                        "classifier": "age/model/name",
+                        "tags": [
+                            {"name": "25 32",
+                             "probability": <float>
+                            }
+                        ]                                            
+                    ]
+                }
+            ],
+            "person" : [
+                {
+                    "path" : "/yet/another/image",
+                    "probability" : <float>,
+                    "bbox" : [x, y, w, h],
+                    "cropped_image": "/yet/another/cropped/image"
+                    "classification": [
+                        "classifier": "gender/model/name",
+                        "tags": [
+                            {"name": "f",
+                             "probability": <float>
+                            },
+                            {"name": "m",
+                             "pobability": <float>
+                            }
+                        ],                    
+                        "classifier": "age/model/name",
+                        "tags": [
+                            {"name": "25 32",
+                             "probability": <float>
+                            }
+                        ]                                            
+                    ]
+                }
+            ]
+            "dog" : [
+                {
+                    "path" : "/yet/another/image",
+                    "probability" : <float>,
+                    "bbox" : [x, y, w, h],
+                    "cropped_image": "/yet/another/cropped/image"
+                }
+            ]
+        }
+    }
+}
 
 """
 import numpy as np
