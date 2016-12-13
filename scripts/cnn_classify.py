8df05e257f70cbd1faaac2897fa629b1f4abd061
Patrick Bos <egpbos@gmail.com>
2016-12-13 13:10:53 +0100

    Removed JSON example in header and subcommand stuff

--- scripts/cnn_classify.py
+++ scripts/cnn_classify.py
@@ -1,156 +1,5 @@
 #!/usr/bin/env python
-"""
-The input JSON for a component comming after the cropper component looks like:
 
-{
-    "files" : [
-        "/path/to/image",
-        "/and/another/image"
-    ],
-    {
-        "classes":{
-            "car" : [
-                {
-                    "path" : "/path/to/image",
-                    "probability" : <float>,
-                    "bbox" : [x, y, w, h],
-                    "cropped_image": "/path/to/cropped/image"
-                },
-                {
-                    "path" : "/and/another/image",
-                    "probability" : <float>,
-                    "bbox" : [x, y, w, h],
-                    "cropped_image": "/another/cropped/image"
-                }
-            ],
-            "person" : [
-                {
-                    "path" : "/yet/another/image",
-                    "probability" : <float>,
-                    "bbox" : [x, y, w, h],
-                    "cropped_image": "/yet/another/cropped/image"
-                }
-            ]
-            "dog" : [
-                {
-                    "path" : "/yet/another/image",
-                    "probability" : <float>,
-                    "bbox" : [x, y, w, h],
-                    "cropped_image": "/yet/another/cropped/image"
-                }
-            ]            
-        }
-    }
-}
-
-For the Bounding Boxes, the (x,y)  coordinates refer to the top left corner of 
-the bounding box.
-
-In addition, the script takes two more arguments: <class (list of string)> and
-<threshold (number)>, e.g. "car" and "0.8". The class ilist of strings are the
-class keys of interest in the input JSON file and the threshold is the minimum 
-class probability above which the class probabilities should be reported in the
-output JSON file.
-
-The output JSON enriches the input JSON with a classifier results for each BBox/
-cropped_image:
-
-{
-    "files" : [
-        "/path/to/image",
-        "/and/another/image"
-    ],
-    {
-        "classes":{
-            "car" : [
-                {
-                    "path" : "/path/to/image",
-                    "probability" : <float>,
-                    "bbox" : [x, y, w, h],
-                    "cropped_image": "/path/to/cropped/image",
-                     "classification": [
-                        "classifier": "car/model/name",
-                        "tags": [
-                            {"name": "Ford Fiesta",
-                             "probability": <float>
-                            },
-                            {"name": "Opel Astra",
-                             "pobability": <float>
-                            }
-                        ],                    
-                        "classifier": "car/color/name",
-                        "tags": [
-                            {"name": "white",
-                             "probability": <float>
-                            },
-                            {"name": "gray",
-                             "probability": <float>
-                            }
-                        ]                                            
-                    ]
-                },
-                {
-                    "path" : "/and/another/image",
-                    "probability" : <float>,
-                    "bbox" : [x, y, w, h],
-                    "cropped_image": "/another/cropped/image",
-                     "classification": [
-                        "classifier": "gender/model/name",
-                        "tags": [
-                            {"name": "f",
-                             "probability": <float>
-                            },
-                            {"name": "m",
-                             "pobability": <float>
-                            }
-                        ],                    
-                        "classifier": "age/model/name",
-                        "tags": [
-                            {"name": "25 32",
-                             "probability": <float>
-                            }
-                        ]                                            
-                    ]
-                }
-            ],
-            "person" : [
-                {
-                    "path" : "/yet/another/image",
-                    "probability" : <float>,
-                    "bbox" : [x, y, w, h],
-                    "cropped_image": "/yet/another/cropped/image"
-                    "classification": [
-                        "classifier": "gender/model/name",
-                        "tags": [
-                            {"name": "f",
-                             "probability": <float>
-                            },
-                            {"name": "m",
-                             "pobability": <float>
-                            }
-                        ],                    
-                        "classifier": "age/model/name",
-                        "tags": [
-                            {"name": "25 32",
-                             "probability": <float>
-                            }
-                        ]                                            
-                    ]
-                }
-            ]
-            "dog" : [
-                {
-                    "path" : "/yet/another/image",
-                    "probability" : <float>,
-                    "bbox" : [x, y, w, h],
-                    "cropped_image": "/yet/another/cropped/image"
-                }
-            ]
-        }
-    }
-}
-
-"""
 import numpy as np
 # import matplotlib.pyplot as plt
 import sys
@@ -354,12 +203,12 @@ if __name__ == '__main__':
     parser.add_argument("-v", "--verbose", help="Verbose mode.", action="store_true", default=0)
 
     # CWL workflow mode
-    subparsers = parser.add_subparsers(dest='subcommand', help='sub-command help')
-    parser_workflow = subparsers.add_parser('workflow', help='workflow-mode help')
+    # subparsers = parser.add_subparsers(dest='subcommand', help='sub-command help')
+    # parser_workflow = subparsers.add_parser('workflow', help='workflow-mode help')
 
-    parser_workflow.add_argument("json_file", help="The filename (including path, full or relative) of the json file with the input. This will usually be specified by the CWL workflow.", type=argparse.FileType('r'))
-    parser_workflow.add_argument("class", help="The class of objects from the input JSON file that will be processed by the specific classifier in the CWL component.", type=str)
-    parser_workflow.add_argument("threshold", help="A cut-off threshold for the probabilities of the classifications.", type=float)
+    # parser_workflow.add_argument("json_file", help="The filename (including path, full or relative) of the json file with the input. This will usually be specified by the CWL workflow.", type=argparse.FileType('r'))
+    # parser_workflow.add_argument("class", help="The class of objects from the input JSON file that will be processed by the specific classifier in the CWL component.", type=str)
+    # parser_workflow.add_argument("threshold", help="A cut-off threshold for the probabilities of the classifications.", type=float)
 
     args = parser.parse_args()
     print args
