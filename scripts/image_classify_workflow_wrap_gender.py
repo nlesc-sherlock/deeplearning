d240b32f03832945b1b76507b0b2cb554985eb88
Patrick Bos <egpbos@gmail.com>
2016-12-13 16:52:26 +0100

    More debugging

--- scripts/image_classify_workflow_wrap_gender.py
+++ scripts/image_classify_workflow_wrap_gender.py
@@ -3,7 +3,7 @@
 # @Author: Patrick Bos
 # @Date:   2016-12-13 11:48:22
 # @Last Modified by:   Patrick Bos
-# @Last Modified time: 2016-12-13 16:40:41
+# @Last Modified time: 2016-12-13 16:52:14
 
 import cnn_classify
 import argparse
@@ -87,6 +87,7 @@ if __name__ == '__main__':
                              json=args.json, outfile=outfile)
 
         with file(outfn, "r") as fp:
+            print(fp.readlines())
             gender_classification = json.load(fp)
 
         print(gender_classification)
