0cc1d755b1544fc7e6a5fa278035cbbf6ff066d3
Patrick Bos <egpbos@gmail.com>
2016-12-13 16:36:16 +0100

    Again fixing output file stuff

--- scripts/image_classify_workflow_wrap_gender.py
+++ scripts/image_classify_workflow_wrap_gender.py
@@ -3,7 +3,7 @@
 # @Author: Patrick Bos
 # @Date:   2016-12-13 11:48:22
 # @Last Modified by:   Patrick Bos
-# @Last Modified time: 2016-12-13 16:32:20
+# @Last Modified time: 2016-12-13 16:35:39
 
 import cnn_classify
 import argparse
@@ -67,24 +67,26 @@ if __name__ == '__main__':
 
     # hard code the json output file, since we need to add this back into the
     # giant workflow json object
-    outfile = file("/tmp/gender_classification.json", "w")
+    outfn = "/tmp/gender_classification.json"
 
     input_json = json.load(args.json_input_file)
 
     image_filenames = get_person_face_image_filenames_from_json(input_json)
 
     if image_filenames:
-        cnn_classify.run(image_filenames, args.model_path, args.model_snapshot,
-                         model_deploy=args.model_deploy,
-                         labels_name=args.model_labels,
-                         mean_pixel_name=args.mean_pixel_name,
-                         gray_range=args.gray_range,
-                         channel_swap=args.channel_swap,
-                         batch_size=args.batch_size,
-                         gpu_id=args.gpu_id, verbose=args.verbose,
-                         json=args.json, outfile=outfile)
+        with file(outfn, "w") as outfile:
+            cnn_classify.run(image_filenames, args.model_path,
+                             args.model_snapshot,
+                             model_deploy=args.model_deploy,
+                             labels_name=args.model_labels,
+                             mean_pixel_name=args.mean_pixel_name,
+                             gray_range=args.gray_range,
+                             channel_swap=args.channel_swap,
+                             batch_size=args.batch_size,
+                             gpu_id=args.gpu_id, verbose=args.verbose,
+                             json=args.json, outfile=outfile)
 
-        with file("/tmp/gender_classification.json", "r") as fp:
+        with file(outfn, "r") as fp:
             gender_classification = json.load(fp)
 
         output_json = generate_output_json(input_json, gender_classification)
