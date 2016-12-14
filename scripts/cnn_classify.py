92c37770955043ab10be73dc359433ec20b13f3a
Patrick Bos <egpbos@gmail.com>
2016-12-14 10:36:11 +0100

    Fix for json output

--- scripts/cnn_classify.py
+++ scripts/cnn_classify.py
@@ -145,7 +145,8 @@ def print_json_classification(probs, image_files, model_path, model_name,
         ix_topN = probs[ix].argsort()[::-1][:ind]
         # need pure python floats for json.dumps serialization:
         topN_probs = [float(p) for p in probs[ix][ix_topN]]
-        topN_classes = zip(labels[ix_topN], topN_probs)
+        topN_labels = [str(l) for l in labels[ix_topN]]
+        topN_classes = zip(topN_labels, topN_probs)
         # tags = "%s" % dict(topN_classes)
         tags = dict(topN_classes)
         data["predictions"][image_file] = {
