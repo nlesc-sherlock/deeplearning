8cbdf188cf393648259a756b64b37a592bec5f60
Patrick Bos <egpbos@gmail.com>
2016-12-14 17:36:01 +0100

    Replaced previous symlinks with copies of scripts

---
+++ docker-workflow/face_detector_minimal/face_detection_workflow_wrap.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Elena Ranguelova
+# @Date:   2016-12-14 11:23
+
+#import sys
+#sys.path.insert(0,'/home/elena/Sherlock/deeplearning/FaceDetector/facedetect-master/')
+import facedetect as fd
+import wrap_lib as wl
+import json
+
+if __name__ == '__main__':
+    parser = wl.get_workflow_argument_parser()
+
+    args = parser.parse_args()
+   # print args
+
+    fd.load_cascades(fd.DATA_DIR)
+    
+    input_json = json.load(args.json_input_file)
+    output_json = input_json
+
+    
+    if 'person' in output_json['classes'].keys():
+        persons = output_json['classes']['person']
+        for person in persons:
+            image_fn = person['cropped_image']
+            _, features = fd.face_detect_file(image_fn)
+            person[u'face'] = {}
+            person[u'face'][u'path'] = image_fn
+            person[u'face'][u'bbox'] = [int(x) for x in list(features[0])]
+            
+    json.dump(output_json, args.workflow_out, indent=4)    
