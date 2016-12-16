7173829f1e19bb791912bd0158226269faefbf45
Berend Weel <b.weel@esciencecenter.nl>
2016-12-16 15:52:41 +0100

    Added two more versions of the ssd detector

---
+++ docker/object_detect_ssdnet/voc/detect.py
@@ -0,0 +1,154 @@
+#!/usr/bin/python
+
+import numpy as np
+
+# Make sure that caffe is on the python path:
+caffe_root = '/opt/caffe/'
+import os
+os.chdir(caffe_root)
+import sys
+sys.path.insert(0, 'python')
+
+os.environ["GLOG_minloglevel"] = "2" # Surpress a bunch of warnings
+import caffe
+caffe.set_device(0)
+caffe.set_mode_gpu()
+
+import json
+
+from google.protobuf import text_format
+from caffe.proto import caffe_pb2
+
+
+def load_labelmap_from_file(labelmap_file):
+    file = open(labelmap_file, 'r')
+    labelmap = caffe_pb2.LabelMap()
+    text_format.Merge(str(file.read()), labelmap)
+    return labelmap
+
+def get_labelname(labelmap, labels):
+    num_labels = len(labelmap.item)
+    labelnames = []
+    if type(labels) is not list:
+        labels = [labels]
+    for label in labels:
+        found = False
+        for i in xrange(0, num_labels):
+            if label == labelmap.item[i].label:
+                found = True
+                labelnames.append(labelmap.item[i].display_name)
+                break
+        assert found == True
+    return labelnames
+
+def load_model():
+    model_def = '/models/VGGNet/VOC0712Plus/SSD_512x512_ft/deploy.prototxt'
+    model_weights = '/models/VGGNet/VOC0712Plus/SSD_512x512_ft/VGG_VOC0712Plus_SSD_512x512_ft_iter_160000.caffemodel'
+
+    net = caffe.Net(model_def,      # defines the structure of the model
+                    model_weights,  # contains the trained weights
+                    caffe.TEST)     # use test mode (e.g., don't perform dropout)
+    return net
+
+def create_transformer(net):
+    # input preprocessing: 'data' is the name of the input blob == net.inputs[0]
+    transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+    transformer.set_transpose('data', (2, 0, 1))
+    transformer.set_mean('data', np.array([104,117,123])) # mean pixel
+    transformer.set_raw_scale('data', 255)  # the reference model operates on images in [0,255] range instead of [0,1]
+    transformer.set_channel_swap('data', (2,1,0))  # the reference model has channels in BGR order instead of RGB
+    return transformer
+
+
+def detect_objects(image_paths, threshold, outfile, verbose=False):
+    net = load_model()
+    transformer = create_transformer(net)
+
+    labelmap = load_labelmap_from_file(caffe_root+'data/VOC0712/labelmap_voc.prototxt')
+
+    # set net batch size to 1
+    image_resize = 512
+    net.blobs['data'].reshape(1,3,image_resize,image_resize)
+
+    input_images = []
+    output = {}
+    output['classifications'] = []
+    for image_file in image_paths:
+        image = caffe.io.load_image(image_file)
+        transformed_image = transformer.preprocess('data', image)
+
+        net.blobs['data'].data[...] = transformed_image
+        detections = net.forward()['detection_out']
+        class_json = classify_object(image_file, image, detections, labelmap, threshold)
+        output['classifications'].append(class_json)
+
+    json_string = json.dumps(output, sort_keys=True, indent=4, ensure_ascii=False)
+    if outfile is not None:
+        outfile.write(json_string)
+    elif verbose:
+        print(json_string)
+
+
+def classify_object(image_file, image, detections, labelmap, threshold, verbose=False):
+    # Parse the outputs.
+    det_label = detections[0,0,:,1]
+    det_conf = detections[0,0,:,2]
+    det_xmin = detections[0,0,:,3]
+    det_ymin = detections[0,0,:,4]
+    det_xmax = detections[0,0,:,5]
+    det_ymax = detections[0,0,:,6]
+
+    # Get detections with confidence higher than 0.6.
+    top_indices = [i for i, conf in enumerate(det_conf) if conf >= threshold]
+
+    top_conf = det_conf[top_indices]
+    top_label_indices = det_label[top_indices].tolist()
+    top_labels = get_labelname(labelmap, top_label_indices)
+    top_xmin = det_xmin[top_indices]
+    top_ymin = det_ymin[top_indices]
+    top_xmax = det_xmax[top_indices]
+    top_ymax = det_ymax[top_indices]
+
+    output = {}
+    output['path'] = image_file
+    if verbose:
+        print(image_file)
+    for i in xrange(top_conf.shape[0]):
+        xmin = int(round(top_xmin[i] * image.shape[1]))
+        ymin = int(round(top_ymin[i] * image.shape[0]))
+        xmax = int(round(top_xmax[i] * image.shape[1]))
+        ymax = int(round(top_ymax[i] * image.shape[0]))
+        score = top_conf[i]
+        label = int(top_label_indices[i])
+        label_name = top_labels[i]
+
+        output['class']  = label_name
+        output['probability'] = float(score)
+        output['bbox'] = [float(xmin), float(ymin), float(xmax-xmin+1), float(ymax-ymin+1)]
+        
+        if verbose:
+            print('%s: %.2f'%(label_name, score), 'bbox: %i, %i, %i, %i'%(xmin, ymin, xmax-xmin+1, ymax-ymin+1))
+
+    return output
+
+
+if __name__ == '__main__':
+    import argparse
+    
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("image_files", help="The filename(s) (including path, full or relative) of the image(s) you want to classify.", nargs="+")
+
+    parser.add_argument("-D", "--data_path", help="Directory path of where the data is mounted. If this script is running whithin a docker it should be the docker local path", default="/data")
+
+    parser.add_argument("-t", "--threshold", type=float, help="Threshold to use for output of classes.", required=True)
+    parser.add_argument("-v", "--verbose", help="Verbose mode.", action='store_true')
+
+    args = parser.parse_args()
+
+    data_path = args.data_path
+    verbose = args.verbose
+    image_filenames = [os.path.join(data_path, image_file) for image_file in args.image_files]
+    
+    threshold = args.threshold
+    detect_objects(image_filenames, threshold, None, verbose=True) 
--- scripts/image_classify_workflow.py
+++ scripts/image_classify_workflow.py
@@ -13,6 +13,13 @@ import shutil
 
 from utils.pathtype import PathType
 
+def check_input(inputfile):
+    stats = os.stat(inputfile)
+    if stats.st_size > 0:
+        return True
+    else:
+        return False
+
 def create_volume_string(volumes):
     new_string = ""
     for key, volume in volumes.iteritems():
@@ -33,11 +40,15 @@ def generate_json_from_directory(directory, outfile, volumes):
     json.dump(files_json, outfile, indent=4, sort_keys=True)
 
 
-def run_ssd_detection(client, input_json, output_json, volumes):
+def run_ssd_detection(client, input_json, output_json, volumes, verbose):
     # Cannot use the docker client unfortunately, have to use nvidia-docker as a command line
     # client.containers.run('nlescsherlockdl/object_detect_ssdnet_wrapper', volumes=volumes, command=["input_json"])
-    command = "nvidia-docker run " + create_volume_string(volumes)  + " nlescsherlockdl/object_detect_ssdnet_wrapper " + \
-              " --workflow_out " + output_json + " " + input_json
+    extra_params = " "
+    if verbose:
+        extra_params += "-v "
+
+    command = "nvidia-docker run " + create_volume_string(volumes)  + " nlescsherlockdl/object_detect_ssdnet_wrapper_coco " + \
+              " --workflow_out " + output_json + extra_params + input_json
     #print("Command: ", command)
     subprocess.call(command, shell=True)
 
@@ -66,7 +77,7 @@ def run_cnn_classify(client, input_json, output_json, volumes, docker_image):
     # Cannot use the docker client unfortunately, have to use nvidia-docker as a command line
     command = "nvidia-docker run " + create_volume_string(volumes) + docker_image + \
               " --workflow_out " + output_json + " " + input_json
-    #print("Command: ", command)
+    print("Command: ", command)
     subprocess.call(command, shell=True)
 
 
@@ -94,11 +105,16 @@ if __name__ == "__main__":
     parser.add_argument("output_directory", type=pathtype)
     parser.add_argument("temp_directory", type=pathtype)
 
+    parser.add_argument("-v", "--verbose", help="Verbose mode.", action="store_true", default=0) 
+
     args = parser.parse_args()
     
     inputdir = args.input_directory
     outputdir = args.output_directory
     tmpdir = args.temp_directory
+
+    verbose = args.verbose
+
     print("done.")
     #print("Arguments found:", args)
     print("\n\n")
@@ -106,7 +122,7 @@ if __name__ == "__main__":
     print("Now setting up the docker environment..", end="")
     sys.stdout.flush()
     docker_images = [
-        'nlescsherlockdl/object_detect_ssdnet_wrapper',
+        'nlescsherlockdl/object_detect_ssdnet_wrapper_coco',
         'nlescsherlockdl/cropper',
         'nlescsherlockdl/car_color_workflow',
         'nlescsherlockdl/car_model_workflow',
@@ -151,68 +167,100 @@ if __name__ == "__main__":
     print("Now running object detection...", end="")
     sys.stdout.flush()
     input_json = os.path.join(volumes['temp']['bind'], "files.json")
+    input_file = os.path.join(volumes['temp']['path'], "files.json")
     output_json = os.path.join(volumes['temp']['bind'], "detect.json")
-    run_ssd_detection(client, input_json, output_json, volumes)
+    if check_input(input_file):
+        run_ssd_detection(client, input_json, output_json, volumes, verbose)
+    else:
+        raise Exception("Input file " + input_file + " was empty")
     print("done.")
 
     print("Now running cropping...", end="")
     sys.stdout.flush()
     input_json  = os.path.join(volumes['temp']['bind'], "detect.json")
+    input_file = os.path.join(volumes['temp']['path'], "detect.json")
     output_json = os.path.join(volumes['temp']['bind'], "cropped.json")
-    run_cropper_after_ssd(client, input_json, output_json, volumes)
+    if check_input(input_file):
+        run_cropper_after_ssd(client, input_json, output_json, volumes)
+    else:
+        raise Exception("Input file " + input_file + " was empty")
     print("done.")
 
     print("Now running car color classification...", end="")
     sys.stdout.flush()
     input_json  = os.path.join(volumes['temp']['bind'], "cropped.json")
+    input_file = os.path.join(volumes['temp']['path'], "cropped.json")
     output_json = os.path.join(volumes['temp']['bind'], "colored.json")
 
     docker_image = 'nlescsherlockdl/car_color_workflow'
-    run_cnn_classify(client, input_json, output_json, volumes, docker_image)
+    if check_input(input_file):
+        run_cnn_classify(client, input_json, output_json, volumes, docker_image)
+    else:
+        raise Exception("Input file " + input_file + " was empty")
     print("done.")
     
     
     print("Now running car model classification...", end="")
     sys.stdout.flush()
     input_json  = os.path.join(volumes['temp']['bind'], "colored.json")
+    input_file = os.path.join(volumes['temp']['path'], "colored.json")
     output_json = os.path.join(volumes['temp']['bind'], "model.json")
 
     docker_image = 'nlescsherlockdl/car_model_workflow'
-    run_cnn_classify(client, input_json, output_json, volumes, docker_image)
+    if check_input(input_file):
+        run_cnn_classify(client, input_json, output_json, volumes, docker_image)
+    else:
+        raise Exception("Input file " + input_file + " was empty")
     print("done.")
 
     print("Now running face detection...", end="")
     sys.stdout.flush()
     input_json  = os.path.join(volumes['temp']['bind'], "model.json")
+    input_file = os.path.join(volumes['temp']['path'], "model.json")
     output_json = os.path.join(volumes['temp']['bind'], "faces.json")
 
-    run_face_detection(client, input_json, output_json, volumes, docker_image)
+    if check_input(input_file):
+        run_face_detection(client, input_json, output_json, volumes, docker_image)
+    else:
+        raise Exception("Input file " + input_file + " was empty")
     print("done.")
 
     print("Now running cropping...", end="")
     sys.stdout.flush()
     input_json  = os.path.join(volumes['temp']['bind'], "faces.json")
+    input_file = os.path.join(volumes['temp']['path'], "faces.json")
     output_json = os.path.join(volumes['temp']['bind'], "faces_cropped.json")
-    run_cropper_after_face_detection(client, input_json, output_json, volumes)
+    if check_input(input_file):
+        run_cropper_after_face_detection(client, input_json, output_json, volumes)
+    else:
+        raise Exception("Input file " + input_file + " was empty")
     print("done.")
 
 
     print("Now running gender classification...", end="")
     sys.stdout.flush()
     input_json  = os.path.join(volumes['temp']['bind'], "faces_cropped.json")
+    input_file = os.path.join(volumes['temp']['path'], "faces_cropped.json")
     output_json = os.path.join(volumes['temp']['bind'], "gender.json")
 
     docker_image = 'nlescsherlockdl/person_face_gender_workflow'
-    run_cnn_classify(client, input_json, output_json, volumes, docker_image)
+    if check_input(input_file):
+        run_cnn_classify(client, input_json, output_json, volumes, docker_image)
+    else:
+        raise Exception("Input file " + input_file + " was empty")
     print("done.")
 
     print("Now running gender classification...", end="")
     sys.stdout.flush()
     input_json  = os.path.join(volumes['temp']['bind'], "gender.json")
+    input_file = os.path.join(volumes['temp']['path'], "gender.json")
     output_json = os.path.join(volumes['temp']['bind'], "age.json")
 
     docker_image = 'nlescsherlockdl/person_face_age_workflow'
-    run_cnn_classify(client, input_json, output_json, volumes, docker_image)
+    if check_input(input_file):
+        run_cnn_classify(client, input_json, output_json, volumes, docker_image)
+    else:
+        raise Exception("Input file " + input_file + " was empty")
     print("done.")
 
     print("Copying results to output...", end="")
