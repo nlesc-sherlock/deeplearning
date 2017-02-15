3e3adbd060b1bafe1011d45a477adae8585a203f
Patrick Bos <egpbos@gmail.com>
2017-02-15 17:05:35 +0100

    Fix impossible relative paths

--- docker/draw_crop_box/test_docker.sh
+++ docker/draw_crop_box/test_docker.sh
@@ -7,9 +7,9 @@ IMAGE_FN=person_horse.jpg
 BOXES_FN=boxes_example_docker.json
 OUTPUT_FN=person_horse_boxes_test.jpg
 docker run \
-    -v $DATADIR:/data \
-    -v $JSONDIR:/json \
-    -v $OUTDIR:/output \
+    -v $(readlink $DATADIR):/data \
+    -v $(readlink $JSONDIR):/json \
+    -v $(readlink $OUTDIR):/output \
     nlescsherlockdl/draw_crop_box \
     /data/$IMAGE_FN /json/$BOXES_FN
     --output_file /output/$OUTPUT_FN
